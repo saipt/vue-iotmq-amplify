1d756203570aa17f29e7984e94ba6242eb1289e1
saipt <saipt@amazon.com>
2023-11-18 10:07:32 +0530

    add pubsub with vue

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -8,6 +8,11 @@ Amplify.configure(amplifyconfig);
 import { fetchAuthSession } from 'aws-amplify/auth';
 import { PubSub } from '@aws-amplify/pubsub';
 
+import { CONNECTION_STATE_CHANGE, ConnectionState } from '@aws-amplify/pubsub';
+import { Hub } from 'aws-amplify/utils';
+
+window.LOG_LEVEL = 'DEBUG'; 
+console.log("----")
 fetchAuthSession().then((info) => {
   const cognitoIdentityId = info.identityId;
   console.log("----",cognitoIdentityId)
@@ -19,12 +24,21 @@ const pubsub = new PubSub({
     endpoint:
       'wss://a2vh905c4kwt1m-ats.iot.ap-south-1.amazonaws.com/mqtt'
   });
-  console.log("Pubsub outpu", pubsub)
+console.log("Pubsub outpu", pubsub)
 
-  pubsub.subscribe({ topics: 'myTopic' }).subscribe({
-    next: (data) => console.log('Message received', data),
-    error: (error) => console.error(error),
-    complete: () => console.log('Done')
+Hub.listen('pubsub', (data) => {
+  const { payload } = data;
+  if (payload.event === CONNECTION_STATE_CHANGE) {
+    const connectionState = payload.data.connectionState;
+    console.log(connectionState);
+  if (connectionState === "Connected"){
+    pubsub.publish({ topics: 'myTopic', message: { msg: 'Message from Amplify console' }}).then(() => console.log("Published! .....")).catch(console.error);
+  }
+  }
+});
+pubsub.subscribe({ topics: 'myTopic' }).subscribe({
+  next: (data) => console.log('Message received', data),
+  error: (error) => console.error(error),
+  complete: () => console.log('Done')
   });
-pubsub.publish({ topics: 'myTopic', message: { msg: 'Hello to all subscribers!' }}).then(() => console.log("Published! .....")).catch(console.error);
 createApp(App).mount('#app')
